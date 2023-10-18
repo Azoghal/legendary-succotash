1838ce9eac1d8ecf1d3beb024316c7979f4ae2fa
Sam Beer <sam.beer208@gmail.com>
2023-10-18 21:30:41 +0100

    now its a vite project

--- lodge/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('it renders succotash', () => {
-  render(<App />);
-  const imageElement = screen.getByAltText("Succotash");
-  expect(imageElement).toBeInTheDocument();
-});
