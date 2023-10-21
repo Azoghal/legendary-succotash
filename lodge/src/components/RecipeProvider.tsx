57c79a9c1b900eac48bf2953f126f4d4cd497e64
Sam Beer <sam.beer208@gmail.com>
2023-10-21 21:39:18 +0100

    maybe this will just do the trick

--- lodge/src/components/RecipeProvider.tsx
+++ lodge/src/components/RecipeProvider.tsx
@@ -1,6 +1,11 @@
 import { ChangeEvent, useState } from "react"
 import { useTranslation } from "react-i18next";
 
+// import {ServotashClient} from "../../gen/prototash_pb_service.js" 
+import {ServotashClient} from "../../gen/prototash_grpc_pb"
+import grpc from "@grpc/grpc-js"
+import { RecipeRequest, RecipeReply } from "../../gen/prototash_pb";
+
 interface IRecipeProviderProps{
     
 }
@@ -13,7 +18,18 @@ export default function RecipeProvider(_props: IRecipeProviderProps){
 
     const getRecipe = () => {
         // actually make a client and do le nice things
-        setRecipeStrings(["Bobly"])
+        var client = new ServotashClient('localhost:50051', grpc.credentials.createInsecure())
+        const req: RecipeRequest = new RecipeRequest()
+        req.setMaxtime(20);
+        const bob: grpc.requestCallback<RecipeReply> = function(err, resp) {
+            if (err){
+                console.log(err)
+                return
+            }
+            console.log(resp)
+            setRecipeStrings(resp?.getInstructionsList())
+        };
+        client.recipe(req, bob)
     }
 
     const updateMinutes = (e: ChangeEvent<HTMLInputElement>) => {
@@ -29,7 +45,7 @@ export default function RecipeProvider(_props: IRecipeProviderProps){
             <button onClick={getRecipe}> {t("button.getrecipe")} </button>
             {recipeStrings && (
                 <div>
-                    This is where the recipe will appear, once it works.
+                    {recipeStrings.map((s) => <span>{s}</span>)}
                 </div>
             )}
         </>
