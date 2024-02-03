e2f79043c865b72cada8c567ca0c210655467377
Sam Beer <sam.beer208@gmail.com>
2024-02-03 20:22:22 +0000

    serve html

--- court/src/main.rs
+++ court/src/main.rs
@@ -1,17 +1,32 @@
+use std::path::{Path, PathBuf};
+
+use rocket::{
+    fs::{relative, FileServer, NamedFile},
+    response::Redirect,
+};
+
 #[macro_use]
 extern crate rocket;
 
 #[cfg(test)]
 mod tests;
 
-#[get("/")]
-fn hello() -> String {
-    "hello you".to_string()
+// #[get("/")]
+// fn index() -> Redirect {
+//     Redirect::permanent("/index.html")
+// }
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    let mut path = Path::new(&format!("{}/../lodge", env!("CARGO_MANIFEST_DIR"))).join(file);
+    if path.is_dir() {
+        path.push("index.html");
+    }
+
+    NamedFile::open(path).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![hello])
-        .mount("/hello", routes![hello])
+    rocket::build().mount("/", rocket::routes![files])
 }
