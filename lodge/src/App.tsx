bc49d9bca20776f46a346b975813549be10e3c19
Sam Beer <sam.beer208@gmail.com>
2024-02-11 15:49:45 +0000

    some columnal shenanigans

--- lodge/src/App.tsx
+++ lodge/src/App.tsx
@@ -7,19 +7,40 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://en.wikipedia.org/wiki/Succotash" target="_blank">
-          <img src={SuccotashLogo} className="logo" alt="Succotash Logo" />
-        </a>
+        <div>
+          <a href="https://en.wikipedia.org/wiki/Succotash" target="_blank">
+            <img src={SuccotashLogo} className="logo" alt="Succotash Logo" />
+          </a>
+        </div>
+        <h1>Legendary. Succotash</h1>
+        <Counter val={0}/>
+        <RecipeButton/>
+        {/* https://bulma.io/documentation/columns/options/ */}
+        {/* The above has a cool example where we can have wider or narrower things in each row */}
+        <div className='columns is-multiline is-centered card-holder'>
+          <div className='column is-3'>
+            <Card title='Tool 1' body='This tool will do a thing'/>
+          </div>
+          <div className='column is-3'>
+            <Card title='Tool 1' body='This tool will do a thing'/>
+          </div>
+          <div className='column is-3'>
+            <Card title='Tool 1' body='This tool will do a thing'/>
+          </div>
+          <div className='column is-3'>
+            <Card title='Tool 1' body='This tool will do a thing'/>
+          </div>
+          <div className='column is-3'>
+            <Card title='Tool 1' body='This tool will do a thing'/>
+          </div>
+          <div className='column is-3'>
+            <Card title='Tool 1' body='This tool will do a thing'/>
+          </div>
+          <div className='column is-3'>
+            <Card title='Tool 1' body='This tool will do a thing'/>
+          </div>
+        </div>
       </div>
-      <h1>Legendary. Succotash</h1>
-      <Counter val={0}/>
-      <RecipeButton/>
-      <div className='columns'>
-        <Card title='Tool 1' body='This tool will do a thing'/>
-        <Card title='Tool 1' body='This tool will do a thing'/>
-        <Card title='Tool 1' body='This tool will do a thing'/>
-      </div>
-      
     </>
   )
 }
